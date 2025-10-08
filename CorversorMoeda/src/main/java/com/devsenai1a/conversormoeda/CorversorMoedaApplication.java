8208d2ee6da5c10a41ff8971346de50a15ea9500
JuAnselmo46 <juliana.anselmo1a@gmail.com>
2025-10-08 15:16:20 -0300

    Add files via upload

---
+++ CorversorMoeda/src/main/java/com/devsenai1a/conversormoeda/CorversorMoedaApplication.java
@@ -0,0 +1,13 @@
+package com.devsenai1a.conversormoeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorversorMoedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorversorMoedaApplication.class, args);
+	}
+
+}
---
+++ CorversorMoeda/src/test/java/com/devsenai1a/conversormoeda/CorversorMoedaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsenai1a.conversormoeda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorversorMoedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
