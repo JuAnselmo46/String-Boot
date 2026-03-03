722193ce3d2c56a22b8f0e2095923708f961b52d
JuAnselmo46 <juliana.anselmo1a@gmail.com>
2026-03-03 13:14:37 -0300

    Sistema Cadastro - Spring Boot 03/03/26

---
+++ cadastro/src/main/java/com/juliana/cadastro/repositories/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.juliana.cadastro.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.juliana.cadastro.entities.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+   // Método para buscar usuário pelo email
+Usuario findByEmail(String email);
+}
